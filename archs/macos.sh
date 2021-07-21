e4da09b2b472118d67e632f2355536128b1318d0
Torrekie <me@torrekie.dev>
2021-07-21 22:07:01 +0800

    add version min arg

--- archs/macos.sh
+++ archs/macos.sh
@@ -2,10 +2,10 @@
 export MACOSX_DEPLOYMENT_TARGET=10.12
 if [ $(uname) == "Darwin" ] && [ $(uname -m | grep -E "x86_64|arm64" > /dev/null ; echo ${?}) -eq 0 ]
 then
-  exec "$(xcrun --sdk macosx -f clang)" -target x86_64-apple-darwin -target arm64-apple-darwin -isysroot "$(xcrun --sdk macosx --show-sdk-path)" $@
+  exec "$(xcrun --sdk macosx -f clang)" -target x86_64-apple-darwin -target arm64-apple-darwin -mmacosx-version-min=10.12 -isysroot "$(xcrun --sdk macosx --show-sdk-path)" $@
 elif [ $(uname) == "Darwin" ] && [ $(uname -m | grep -E "iPhone|iPad|iPod" > /dev/null ; echo ${?}) -eq 0 ]
 then
   exec /usr/bin/clang -target x86_64-apple-darwin -target arm64-apple-darwin -mmacosx-version-min=10.12
 else
-  exec $THEOS/toolchain/linux/iphone/bin/clang -F$THEOS/vendor/lib -target x86_64-apple-darwin -target arm64-apple-darwin -isysroot $THEOS/sdks/MacOSX11.0.sdk $@
+  exec $THEOS/toolchain/linux/iphone/bin/clang -target x86_64-apple-darwin -target arm64-apple-darwin -mmacosx-version-min=10.12 -isysroot $THEOS/sdks/MacOSX11.0.sdk $@
 fi
