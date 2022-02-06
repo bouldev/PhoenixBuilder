acfad3653c5c8c9309a0f0d5a7379ac138e6455f
Torrekie <me@torrekie.dev>
2022-02-06 19:26:37 +0800

    Haven't you guys found this shit never builds on iOS?

--- archs/ios.sh
+++ archs/ios.sh
@@ -5,7 +5,7 @@ then
   exec "$(xcrun --sdk iphoneos -f clang)" -target arm64-apple-ios9.0 -target arm64-apple-darwin -isysroot "$(xcrun --sdk iphoneos --show-sdk-path)" $@
 elif [ $(uname) == "Darwin" ] && [ $(uname -m | grep -E "iPhone|iPad|iPod" > /dev/null ; echo ${?}) -eq 0 ]
 then
-  exec /usr/bin/clang -target arm64-apple-ios9.0 -target arm64-apple-darwin
+  exec /usr/bin/clang -target arm64-apple-ios9.0 -target arm64-apple-darwin $@
 else
   exec $THEOS/toolchain/linux/iphone/bin/clang -F$THEOS/vendor/lib -target arm64-apple-ios9.0 -target arm64-apple-darwin -isysroot $THEOS/sdks/iPhoneOS10.3.sdk $@
 fi
