8595714da22f1ce2a39a79358d98c662888f92d3
LNSSPsd <24724395+LNSSPsd@users.noreply.github.com>
2022-02-09 18:57:11 +0800

    Bump version

--- main.go
+++ main.go
@@ -44,7 +44,7 @@ type FBPlainToken struct {
 
 //Version num should seperate from fellow strings
 //for implenting print version feature later
-const FBVersion = "1.3.2"
+const FBVersion = "1.3.3"
 const FBCodeName = "Phoenix"
 
 func main() {
