ea3a0f8bcf96190140919bf1e47668d61cec4654
LNSSPsd <24724395+LNSSPsd@users.noreply.github.com>
2022-02-15 13:17:40 +0800

    Use snprintf to avoid memory overflow.

--- fastbuilder/command/replaceitem.c
+++ fastbuilder/command/replaceitem.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 
 void replaceItemRequestInternal(GoString *preallocatedStr, int x, int y, int z, unsigned char slot, const char *name, unsigned char count, unsigned short damage) {
-	sprintf((char*)preallocatedStr->p,"replaceitem block %d %d %d slot.container %d %s %d %d", x, y, z, slot, name, count, damage);
+	snprintf((char*)preallocatedStr->p,1023,"replaceitem block %d %d %d slot.container %d %s %d %d", x, y, z, slot, name, count, damage);
 	preallocatedStr->n=strlen(preallocatedStr->p);
 }
 
--- fastbuilder/command/setblock.c
+++ fastbuilder/command/setblock.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 
 void SetBlockRequestInternal(GoString *preallocatedStr, int x, int y, int z, const char *blockName, unsigned short data, const char *method) {
-	sprintf((char*)preallocatedStr->p,"setblock %d %d %d %s %d %s",x,y,z,blockName,data,method);
+	snprintf((char*)preallocatedStr->p,1023,"setblock %d %d %d %s %d %s",x,y,z,blockName,data,method);
 	preallocatedStr->n=strlen(preallocatedStr->p);
 }
 
