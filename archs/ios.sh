b9f5f96a5e3e201b0cd58e903dd519c9f0a9aef6
LNSSPsd <24724395+LNSSPsd@users.noreply.github.com>
2022-02-19 13:49:38 +0800

    Theos' iOS 14.4 sdk was replaced by 14.5 sdk.

--- archs/ios.sh
+++ archs/ios.sh
@@ -7,5 +7,5 @@ elif [ $(uname) == "Darwin" ] && [ $(uname -m | grep -E "iPhone|iPad|iPod" > /de
 then
   exec /usr/bin/clang -target arm64-apple-ios9.0 -target arm64-apple-darwin $@
 else
-  exec $THEOS/toolchain/linux/iphone/bin/clang -F$THEOS/vendor/lib -target arm64-apple-ios14.0 -target arm64-apple-darwin -isysroot $THEOS/sdks/iPhoneOS14.4.sdk $@
+  exec $THEOS/toolchain/linux/iphone/bin/clang -F$THEOS/vendor/lib -target arm64-apple-ios14.0 -target arm64-apple-darwin -isysroot $THEOS/sdks/iPhoneOS14.5.sdk $@
 fi
