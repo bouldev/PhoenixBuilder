9548f2bdadd69278a6815828e054b7c3e6636521
LNSSPsd <24724395+LNSSPsd@users.noreply.github.com>
2022-02-19 13:38:52 +0800

    Use iPhoneOS14 as target

--- archs/ios.sh
+++ archs/ios.sh
@@ -7,5 +7,5 @@ elif [ $(uname) == "Darwin" ] && [ $(uname -m | grep -E "iPhone|iPad|iPod" > /de
 then
   exec /usr/bin/clang -target arm64-apple-ios9.0 -target arm64-apple-darwin $@
 else
-  exec $THEOS/toolchain/linux/iphone/bin/clang -F$THEOS/vendor/lib -target arm64-apple-ios9.0 -target arm64-apple-darwin -isysroot $THEOS/sdks/iPhoneOS10.3.sdk -Wl,-U,_memcpy $@
+  exec $THEOS/toolchain/linux/iphone/bin/clang -F$THEOS/vendor/lib -target arm64-apple-ios14.0 -target arm64-apple-darwin -isysroot $THEOS/sdks/iPhoneOS14.4.sdk $@
 fi
