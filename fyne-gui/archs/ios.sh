023409d16bc08c7acb0235fac9f24ed4710cf120
LNSSPsd <24724395+LNSSPsd@users.noreply.github.com>
2022-03-03 11:48:31 +0800

    Auto select sdk.

--- fyne-gui/archs/ios.sh
+++ fyne-gui/archs/ios.sh
@@ -7,5 +7,13 @@ elif [ $(uname) == "Darwin" ] && [ $(uname -m | grep -E "iPhone|iPad|iPod" > /de
 then
   exec /usr/bin/clang -target arm64-apple-ios9.0 -target arm64-apple-darwin $@
 else
-  exec $THEOS/toolchain/linux/iphone/bin/clang -F$THEOS/vendor/lib -target arm64-apple-ios11.0 -isysroot $THEOS/sdks/iPhoneOS12.2.sdk -F$THEOS/sdks/iPhoneOS12.2.sdk/System/Library/PrivateFrameworks -framework Foundation -framework CoreFoundation -framework UIKit -framework CoreGraphics -framework CoreUI -framework AVFoundation -fobjc-arc -Wno-unused-command-line-argument -lSystem $@
+  if [ -e $THEOS/sdks/iPhoneOS12.2.sdk ]; then
+    export SDK_PATH=$THEOS/sdks/iPhoneOS12.2.sdk
+  elif [ -e $THEOS/sdks/iPhoneOS12.4.sdk ]; then
+    export SDK_PATH=$THEOS/sdks/iPhoneOS12.4.sdk
+  else
+    echo SDK not found
+    exit 1
+  fi
+  exec $THEOS/toolchain/linux/iphone/bin/clang -F$THEOS/vendor/lib -target arm64-apple-ios11.0 -isysroot $SDK_PATH -F$SDK_PATH/System/Library/PrivateFrameworks -framework Foundation -framework CoreFoundation -framework UIKit -framework CoreGraphics -framework CoreUI -framework AVFoundation -fobjc-arc -Wno-unused-command-line-argument -lSystem $@
 fi
