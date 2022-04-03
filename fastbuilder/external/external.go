0352769ea8c91b18f2e87f5aa80b32cdfdaaef50
CMA2401PT <1634268014@qq.com>
2022-04-03 21:07:56 +0800

    pointer

--- fastbuilder/external/external.go
+++ fastbuilder/external/external.go
@@ -1,77 +1,77 @@
 package external
 
 import (
+	"fmt"
+	"phoenixbuilder/fastbuilder/environment"
 	"phoenixbuilder/fastbuilder/external/connection"
 	"phoenixbuilder/fastbuilder/external/packet"
-	"phoenixbuilder/fastbuilder/environment"
-	"fmt"
 )
 
 type ExternalConnectionHandler struct {
-	listener *connection.KCPConnectionServerHandler
-	env *environment.PBEnvironment
+	listener      *connection.KCPConnectionServerHandler
+	env           *environment.PBEnvironment
 	PacketChannel chan []byte
 }
 
 func (handler *ExternalConnectionHandler) acceptConnection(conn connection.ReliableConnection) {
-	env:=handler.env
-	allAlive:=true
+	env := handler.env
+	allAlive := true
 	go func() {
 		for {
-			gamePacket:=<-handler.PacketChannel
-			if(!allAlive) {
+			gamePacket := <-handler.PacketChannel
+			if !allAlive {
 				return
 			}
-			packet.SerializeAndSend(packet.GamePacket {
+			packet.SerializeAndSend(&packet.GamePacket{
 				Content: gamePacket,
 			}, conn)
 			select {
-			case handler.PacketChannel<-gamePacket:
+			case handler.PacketChannel <- gamePacket:
 			default:
 			}
 			// Send the packet to the next receiver(connection)
 		}
-	} ()
+	}()
 	go func() {
 		for {
-			rawPacket, err:=conn.RecvFrame()
-			if(err!=nil||!allAlive) {
-				allAlive=false
+			rawPacket, err := conn.RecvFrame()
+			if err != nil || !allAlive {
+				allAlive = false
 				return
 			}
-			pkt, canParse:=packet.Deserialize(rawPacket)
+			pkt, canParse := packet.Deserialize(rawPacket)
 			if !canParse {
-				packet.SerializeAndSend(packet.PacketViolationWarningPacket {
+				packet.SerializeAndSend(&packet.PacketViolationWarningPacket{
 					Text: "Unparsable packet received!",
 				}, conn)
 			}
-			switch p:=pkt.(type) {
-			case packet.PingPacket:
-				packet.SerializeAndSend(packet.PongPacket{},conn)
-			case packet.PongPacket:
+			switch p := pkt.(type) {
+			case *packet.PingPacket:
+				packet.SerializeAndSend(&packet.PongPacket{}, conn)
+			case *packet.PongPacket:
 				break
-			case packet.ByePacket:
-				packet.SerializeAndSend(packet.ByePacket{},conn)
-			case packet.PacketViolationWarningPacket:
+			case *packet.ByePacket:
+				packet.SerializeAndSend(&packet.ByePacket{}, conn)
+			case *packet.PacketViolationWarningPacket:
 				break
-			case packet.EvalPBCommandPacket:
+			case *packet.EvalPBCommandPacket:
 				handler.env.FunctionHolder.Process(p.Command)
-			case packet.GameCommandPacket:
-				if(p.CommandType==packet.CommandTypeSettings) {
+			case *packet.GameCommandPacket:
+				if p.CommandType == packet.CommandTypeSettings {
 					env.CommandSender.SendSizukanaCommand(p.Command)
 					break
-				}else if(p.CommandType==packet.CommandTypeNormal) {
-					env.CommandSender.SendWSCommand(p.Command,p.UUID)
-				}else{
-					env.CommandSender.SendCommand(p.Command,p.UUID)
+				} else if p.CommandType == packet.CommandTypeNormal {
+					env.CommandSender.SendWSCommand(p.Command, p.UUID)
+				} else {
+					env.CommandSender.SendCommand(p.Command, p.UUID)
 				}
 			}
 		}
-	} ()
+	}()
 }
 
 func (_ *ExternalConnectionHandler) acceptConnectionFail(err error) {
-	fmt.Printf("External connection handler: Failed to accept a connection: %v\n",err)
+	fmt.Printf("External connection handler: Failed to accept a connection: %v\n", err)
 }
 
 func (_ *ExternalConnectionHandler) downError(_ interface{}) {
@@ -79,22 +79,22 @@ func (_ *ExternalConnectionHandler) downError(_ interface{}) {
 }
 
 func ListenExt(env *environment.PBEnvironment, address string) {
-	handlerStruct:=&ExternalConnectionHandler {
-		listener: &connection.KCPConnectionServerHandler{},
+	handlerStruct := &ExternalConnectionHandler{
+		listener:      &connection.KCPConnectionServerHandler{},
 		PacketChannel: make(chan []byte),
 	}
-	env.ExternalConnectionHandler=handlerStruct
-	env.Destructors=append(env.Destructors,func() {
+	env.ExternalConnectionHandler = handlerStruct
+	env.Destructors = append(env.Destructors, func() {
 		close(handlerStruct.PacketChannel)
 	})
-	listener:=handlerStruct.listener
-	err:=listener.Listen(address)
-	if(err!=nil) {
-		fmt.Printf("Failed to listen on address %s: %v\n",address,err)
+	listener := handlerStruct.listener
+	err := listener.Listen(address)
+	if err != nil {
+		fmt.Printf("Failed to listen on address %s: %v\n", address, err)
 		return
 	}
 	listener.SetOnNewConnection(handlerStruct.acceptConnection)
 	listener.SetOnAcceptNewConnectionFail(handlerStruct.acceptConnectionFail)
 	listener.SetOnServerDown(handlerStruct.downError)
-	fmt.Printf("Listening for external connection on address %s\n",address)
+	fmt.Printf("Listening for external connection on address %s\n", address)
 }
