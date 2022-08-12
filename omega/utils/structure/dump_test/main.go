20169f640d476e2d4e88d83d932ba73981469630
CMA2401PT <97827666+CMA2401PT@users.noreply.github.com>
2022-08-12 21:40:59 +0800

    mcworld export WIP

---
+++ omega/utils/structure/dump_test/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path"
+	"phoenixbuilder/mirror"
+	"phoenixbuilder/mirror/define"
+	"phoenixbuilder/mirror/io/mcdb"
+	"phoenixbuilder/omega/utils/structure"
+	"strconv"
+	"strings"
+
+	"github.com/df-mc/goleveldb/leveldb/opt"
+)
+
+func ParseSaveCmd(cmds []string) (startPos, endPos define.CubePos, structureName string, err error) {
+	paramsNames := []string{"[建筑名]", "[起点x]", "[起点y]", "[起点z]", "[终点x]", "[终点y]", "[终点z]"}
+	err = fmt.Errorf("保存指令错误, 应该为:\n %v "+strings.Join(paramsNames, " "), cmds[0])
+	values := [6]int{}
+	if len(cmds) < 8 {
+		return
+	}
+	structureName = cmds[1]
+	for i := 0; i < 6; i++ {
+		if v, _err := strconv.Atoi(cmds[i+2]); _err != nil {
+			err = fmt.Errorf(err.Error() + fmt.Sprintf("\n参数 %v 不正确", paramsNames[i+1]))
+		} else {
+			values[i] = v
+		}
+	}
+	sortStart := func(i int) {
+		startPos[i] = values[i]
+		endPos[i] = values[i+3]
+		if values[i] > values[i+3] {
+			startPos[i] = values[i+3]
+			endPos[i] = values[i]
+		}
+	}
+	for i := 0; i < 3; i++ {
+		sortStart(i)
+	}
+	err = nil
+	return
+}
+
+func AcquireData(hopPath *structure.ExportHopPosMap, allRequiredChunks *structure.ExportedChunksMap) (map[define.ChunkPos]*mirror.ChunkData, error) {
+	provider, err := mcdb.New("/Users/dai/projects/PhoenixBuilder/omega_storage/worlds/current", opt.FlateCompression)
+	if err != nil {
+		return nil, err
+	}
+	chunks := make(map[define.ChunkPos]*mirror.ChunkData)
+	for pos, _ := range *allRequiredChunks {
+		if cd := provider.Get(pos); cd != nil {
+			chunks[pos] = cd
+			allRequiredChunks.Hit(pos)
+		} else {
+			// return nil, fmt.Errorf("chunk %v (Pos: %v) not found", pos, define.ChunkPos{pos.X() << 4, pos.Z() << 4})
+		}
+	}
+	if len(*allRequiredChunks) > 0 {
+		return nil, fmt.Errorf("cannot obtain all chunks")
+	}
+	return chunks, nil
+}
+
+func main() {
+	cmdStr := "save test -3 -5 7 103 69 137"
+	cmds := strings.Split(cmdStr, " ")
+	startPos, endPos, structureName, err := ParseSaveCmd(cmds)
+	if err != nil {
+		panic(err)
+	}
+	hopPath, allRequiredChunks := structure.PlanHopSwapPath(startPos.X(), startPos.Z(), endPos.X(), endPos.Z(), 4)
+
+	fmt.Println(hopPath)
+	fmt.Println(allRequiredChunks)
+	fmt.Println(structureName)
+
+	chunks, err := AcquireData(hopPath, allRequiredChunks)
+	if err != nil {
+		panic(err)
+	}
+
+	overallCacheDir := path.Join("omega_export_cache", structureName)
+	if err := os.MkdirAll(overallCacheDir, 0755); err != nil {
+		panic(err)
+	}
+
+	err = structure.EncodeMCWorld(chunks, startPos, endPos, structureName, overallCacheDir)
+	if err != nil {
+		panic(err)
+	}
+}
