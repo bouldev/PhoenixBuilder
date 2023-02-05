e2741f5bbbfc42bcf8d52054f303f0e8b971fd99
awaqwqa <88972629+awaqwqa@users.noreply.github.com>
2023-02-06 00:55:40 +0800

    公会系统 (#247)
    简约一点的公会系统
    可能有点bug 因为有点赶工
    后面再进行修复
    大的bug应该是没有

--- omega/third_party/sunlife/pool.go
+++ omega/third_party/sunlife/pool.go
@@ -22,4 +22,5 @@ var Components = map[string]defines.Component{
 	"Pvp系统":      &Pvp{BasicComponent: &defines.BasicComponent{}},
 	"自定义附魔升级系统":  &Enchant{BasicComponent: &defines.BasicComponent{}},
 	"无前缀简单自定义指令": &CustomCmd{BasicComponent: &defines.BasicComponent{}},
+	"王国系统":       &King{BasicComponent: &defines.BasicComponent{}},
 }
