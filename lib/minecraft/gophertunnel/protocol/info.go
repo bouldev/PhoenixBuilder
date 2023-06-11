40947c5053ca98d696c2fd382376ce436435c8a3
CMA2401PT <97827666+CMA2401PT@users.noreply.github.com>
2023-06-11 21:53:40 +0800

    since the core part is already picked up from fb, these duplicated code are no longer needed

--- lib/minecraft/gophertunnel/protocol/info.go
+++
@@ -1,8 +0,0 @@
-package protocol
-
-const (
-	// CurrentProtocol is the current protocol version for the version below.
-	CurrentProtocol = 504
-	// CurrentVersion is the current version of Minecraft as supported by the `packet` package.
-	CurrentVersion = "1.18.30"
-)
--- lib/minecraft/gophertunnel/protocol/limit.go
+++
@@ -1,5 +0,0 @@
-package protocol
-
-const lowerLimit = 64
-const mediumLimit = 256
-const higherLimit = 1024
