8be98037e942ab90b294b0b831eaf3392f5fb391
Eternal Crystal <109064184+Happy2018new@users.noreply.github.com>
2023-07-05 00:52:18 +0800

    revert "print decode failure"
    Temporary change, otherwise the user will be flooded with error messages.

--- minecraft/packet.go
+++ minecraft/packet.go
@@ -44,14 +44,12 @@ func (p *packetData) decode(conn *Conn) (pk packet.Packet, err error) {
 	r := protocol.NewReader(p.payload, conn.shieldID.Load())
 	defer func() {
 		if recoveredErr := recover(); recoveredErr != nil {
-			//
-			err = fmt.Errorf("%T: %w", pk, recoveredErr.(error))
-			fmt.Println(err)
+			// 
+			//err = fmt.Errorf("%T: %w", pk, recoveredErr.(error))
 		}
 	}()
 	pk.Unmarshal(r)
 	if p.payload.Len() != 0 {
-		fmt.Println(fmt.Errorf("%T: %v unread bytes left: 0x%x", pk, p.payload.Len(), p.payload.Bytes()))
 		return pk, nil
 		//return pk, fmt.Errorf("%T: %v unread bytes left: 0x%x", pk, p.payload.Len(), p.payload.Bytes())
 	}
