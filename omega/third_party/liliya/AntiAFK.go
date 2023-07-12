6eb4a34972b28187d0e05c510dad627da1119015
Liliya233 <79683806+Liliya233@users.noreply.github.com>
2023-07-12 18:07:39 +0800

    Omega-挂机检测组件升级 (#332)
    * Update AFK
    
    * Update AntiAFK.go
    
    * Update default_components.json
    
    ---------
    
    Co-authored-by: CMA2401PT <97827666+CMA2401PT@users.noreply.github.com>

--- omega/third_party/liliya/AntiAFK.go
+++ omega/third_party/liliya/AntiAFK.go
@@ -13,13 +13,18 @@ import (
 
 type AntiAFK struct {
 	*defines.BasicComponent
-	Duration int         `json:"检测周期"`
+	Duration int         `json:"检测周期_秒"`
 	CmdsIn   interface{} `json:"符合条件时执行的指令"`
 	cmds     []defines.Cmd
 	lastYRot map[uuid.UUID]float64
 }
 
 func (o *AntiAFK) Init(cfg *defines.ComponentConfig, storage defines.StorageAndLogProvider) {
+	if cfg.Version == "0.0.1" {
+		cfg.Configs["检测周期_秒"] = cfg.Configs["检测周期"].(float64) * 60
+		cfg.Version = "0.0.2"
+		cfg.Upgrade()
+	}
 	marshal, _ := json.Marshal(cfg.Configs)
 	if err := json.Unmarshal(marshal, o); err != nil {
 		panic(err)
@@ -39,7 +44,7 @@ func (o *AntiAFK) Inject(frame defines.MainFrame) {
 }
 
 func (o *AntiAFK) Activate() {
-	t := time.NewTicker(time.Minute * time.Duration(o.Duration))
+	t := time.NewTicker(time.Second * time.Duration(o.Duration))
 	for {
 		<-t.C
 		o.Frame.GetGameControl().SendCmdAndInvokeOnResponse("querytarget @a", func(output *packet.CommandOutput) {
@@ -64,7 +69,8 @@ func (o *AntiAFK) Activate() {
 						}
 						if v, ok := o.lastYRot[u.UUID]; ok && (v == u.YRot || math.Abs(v)+math.Abs(u.YRot) == 360) {
 							replacement := map[string]interface{}{
-								"[player]": "\"" + playerName + "\"",
+								"[player]":                   "\"" + playerName + "\"",
+								"[player_without_quotation]": playerName,
 							}
 							go utils.LaunchCmdsArray(o.Frame.GetGameControl(), o.cmds, replacement, o.Frame.GetBackendDisplay())
 						} else {
