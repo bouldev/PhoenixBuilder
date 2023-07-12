6211202d59b39853c5a5cf06170105e7a59efc73
CMA2401PT <97827666+CMA2401PT@users.noreply.github.com>
2023-07-12 19:32:18 +0800

    simplify lua async ctrl

--- solutions/omega_lua/omega_lua/concurrent/async_ctrl.go
+++ solutions/omega_lua/omega_lua/concurrent/async_ctrl.go
@@ -10,35 +10,23 @@ import (
 // 异步管理器
 type AsyncCtrl struct {
 	*sync.WaitGroup
-	closed   bool
-	ctx      context.Context
-	cancelFn func()
-	mu       sync.Mutex
+	closed bool
+	ctx    context.Context
+	mu     sync.Mutex
 }
 
 // 获取一个新的异步管理器
-func NewAsyncCtrl(baseCtx context.Context) *AsyncCtrl {
-	if baseCtx == nil {
-		baseCtx = context.Background()
+func NewAsyncCtrl(ctx context.Context) *AsyncCtrl {
+	if ctx == nil {
+		ctx = context.Background()
 	}
-	ctx, cancelFn := context.WithCancel(baseCtx)
-
 	a := &AsyncCtrl{
 		WaitGroup: &sync.WaitGroup{},
 		closed:    false,
 		ctx:       ctx,
-		cancelFn:  cancelFn,
 		mu:        sync.Mutex{},
 	}
-	go func() {
-		<-ctx.Done()    // 监听 ctx 的完成事件
-		a.closed = true // 标记异步管理器已关闭
-	}()
 	return a
-}
-
-func (a *AsyncCtrl) IsClosed() bool {
-	return a.closed
 }
 
 // 取得上下文
@@ -48,35 +36,33 @@ func (a *AsyncCtrl) Context() context.Context {
 
 // 锁住解释器
 // The LState is not goroutine-safe. It is recommended to use one LState per goroutine and communicate between goroutines by using channels.
-func (a *AsyncCtrl) LockLState() {
+func (a *AsyncCtrl) lockLState() {
 	a.mu.Lock()
 }
 
 // 解锁解释器
-func (a *AsyncCtrl) UnlockLState() {
+func (a *AsyncCtrl) unlockLState() {
 	a.mu.Unlock()
 }
 
 // 安全地调用lua函数 第一个参数为解释器 第二个参数为函数 第三个参数为函数的参数
 func (a *AsyncCtrl) SafeCall(L *lua.LState, cp lua.P, args ...lua.LValue) (err error) {
-	if a.IsClosed() {
+	if a.ctx.Err() != nil {
 		return nil
 	}
-	a.LockLState()
-	defer a.UnlockLState()
+	a.lockLState()
+	defer a.unlockLState()
 	return L.CallByParam(cp, args...)
 }
 
-// 创建一个新的go线程并且请求完成指定函数
-// 必须先完成
-// 如果不完成则堵塞
-func (a *AsyncCtrl) NewGoRoutine(fn func()) {
-	if a.IsClosed() {
-		return
+func (a *AsyncCtrl) AddCoro(count int) error {
+	if a.ctx.Err() != nil {
+		return a.ctx.Err()
 	}
-	a.Add(1) // 添加计数器
-	go func() {
-		defer a.Done() // 减少计数器
-		fn()           // 执行传入的函数
-	}()
+	a.Add(count)
+	return nil
+}
+
+func (a *AsyncCtrl) DecreaseCoro() {
+	a.Done()
 }
--- solutions/omega_lua/omega_lua/concurrent/fire.go
+++ solutions/omega_lua/omega_lua/concurrent/fire.go
@@ -6,22 +6,21 @@ import (
 
 // FireLuaCodeInGoRoutine 在一个新的 Go 协程中执行给定的 Lua 代码，并返回一个错误通道
 // 热启动lua代码
-func FireLuaCodeInGoRoutine(ctx *AsyncCtrl, L *lua.LState, code string) (done <-chan error) {
+func FireLuaCodeInGoRoutine(ac *AsyncCtrl, L *lua.LState, code string) (done <-chan error) {
 	// 创建一个带缓冲的错误通道 用于取消这个线程
 	//办到中断这个lua代码
 	doneChan := make(chan error, 1)
+	var err error
+	ac.AddCoro(1)
 	go func() {
-		// 将上下文设置到 LState 中
-		L.SetContext(ctx.Context())
-		// 锁住整个解释器
-		ctx.LockLState()
-		err := L.DoString(code)
-		//解锁解释器
-		ctx.UnlockLState()
-		// wait until all async call finished
-		//当所有的异步都加载完毕再返回取消消息
-		ctx.WaitGroup.Wait()
+		defer ac.DecreaseCoro()
+		L.SetContext(ac.Context())
+		err = L.DoString(code)
+	}()
+	go func() {
+		ac.WaitGroup.Wait()
 		doneChan <- err
 	}()
+
 	return doneChan
 }
