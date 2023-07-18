9deb6b12c48fb2804c240d405addd7bdbe3f0869
CMA2401PT <1634268014@qq.com>
2023-07-18 23:38:24 +0800

    remove all omega components which is fully written by CMA2401PT

--- solutions/omega_lua/omega_lua/lua_utils/config.go
+++
@@ -1,6 +0,0 @@
-package lua_utils
-
-type LuaConfigRaw struct {
-	Config         interface{}
-	OnConfigUpdate func(interface{})
-}
--- solutions/omega_lua/omega_lua/modules/packets_utils/define.go
+++
@@ -1,9 +0,0 @@
-package packets_utils
-
-import (
-	"phoenixbuilder/solutions/omega_lua/omega_lua/mux_pumper"
-)
-
-type LuaGoPackets interface {
-	GetMCPacketNameIDMapping() mux_pumper.MCPacketNameIDMapping
-}
