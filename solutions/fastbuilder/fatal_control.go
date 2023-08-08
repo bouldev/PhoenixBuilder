8a5c3a2291bfad788c0a598d96ef41b52023fcf8
CMA2401PT <1634268014@qq.com>
2023-08-08 17:47:14 +0800

    remove omega except 3rd  component

--- solutions/fastbuilder/fatal_control.go
+++ solutions/fastbuilder/fatal_control.go
@@ -2,18 +2,17 @@ package fastbuilder
 
 import (
 	"bufio"
-	"fmt"
 	"os"
 	"phoenixbuilder/fastbuilder/args"
 	"phoenixbuilder/fastbuilder/readline"
-	"phoenixbuilder/omega/suggest"
 	"runtime"
 	"runtime/debug"
 
 	I18n "phoenixbuilder/fastbuilder/i18n"
 
-	"github.com/pterm/pterm"
 	_ "unsafe"
+
+	"github.com/pterm/pterm"
 )
 
 var PassFatal bool = false
@@ -33,10 +32,6 @@ func Fatal() {
 		pterm.Error.Println(I18n.T(I18n.Crashed_Tip))
 		pterm.Error.Println(I18n.T(I18n.Crashed_StackDump_And_Error))
 		pterm.Error.Println(err)
-		if args.ShouldEnableOmegaSystem {
-			omegaSuggest := suggest.GetOmegaErrorSuggest(fmt.Sprintf("%v", err))
-			fmt.Print(omegaSuggest)
-		}
 		if runtime.GOOS == "windows" {
 			pterm.Error.Println(I18n.T(I18n.Crashed_OS_Windows))
 			_, _ = bufio.NewReader(os.Stdin).ReadString('\n')
