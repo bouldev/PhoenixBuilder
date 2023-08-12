f46375b4090ee1daaf1be716c79ad84286c4754e
Happy2018new <chenbo79800@163.com>
2023-08-12 21:36:20 +0800

    minor changes on container operations

--- game_control/game_interface/container_operations.go
+++ game_control/game_interface/container_operations.go
@@ -14,7 +14,7 @@ hotBarSlotID 字段代表玩家此时手持的物品栏，
 容器不一定总能打开，可能该容器已被移除或机器人已被移动。
 因此，单次打开操作在抵达最长截止时间后将会在内部被验证为超时，
 此时将会重新提交一次容器打开操作，
-直到总操作次数抵达 ContainerOpenReTryMaximumCounts 时止。
+直到总操作次数抵达 ContainerOperationsReTryMaximumCounts 时止。
 
 请确保在使用此函数前占用了容器资源，否则会造成程序 panic
 */
@@ -24,7 +24,11 @@ func (g *GameInterface) OpenContainer(
 	blockStates map[string]interface{},
 	hotBarSlotID uint8,
 ) (bool, error) {
-	for i := 0; i < ContainerOpenReTryMaximumCounts; i++ {
+	if g.Resources.Container.GetContainerOpeningData() != nil {
+		return false, ErrContainerHasBeenOpened
+	}
+	// if the container has been opened
+	for i := 0; i < ContainerOperationsReTryMaximumCounts; i++ {
 		g.Resources.Container.AwaitChangesBeforeSendingPacket()
 		// await responce before send packet
 		err := g.ClickBlock(
@@ -49,7 +53,7 @@ func (g *GameInterface) OpenContainer(
 		// return
 	}
 	// open container.
-	// try a maximum of ContainerOpenReTryMaximumCounts times
+	// try a maximum of ContainerOperationsReTryMaximumCounts times
 	return false, nil
 	// return
 }
@@ -58,29 +62,40 @@ func (g *GameInterface) OpenContainer(
 关闭已经打开的容器，且只有当容器被关闭后才会返回值。
 您应该确保容器被关闭后，对应的容器公用资源被释放。
 
-返回值的第一项代表执行结果，为真时容器被成功关闭，否则反之
+返回值的第一项代表执行结果，为真时容器被成功关闭，否则反之。
+
+容器不一定总能关闭，可能租赁服已经卡死。
+因此，单次关闭操作在抵达最长截止时间后将会在内部被验证为超时，
+此时将会重新提交一次容器打开操作，
+直到总操作次数抵达 ContainerOperationsReTryMaximumCounts 时止。
 */
 func (g *GameInterface) CloseContainer() (bool, error) {
 	if g.Resources.Container.GetContainerOpeningData() == nil {
 		return false, ErrContainerNerverOpened
 	}
-	// if the container have been nerver opened
-	g.Resources.Container.AwaitChangesBeforeSendingPacket()
-	// await responce before send packet
-	err := g.WritePacket(&packet.ContainerClose{
-		WindowID:   g.Resources.Container.GetContainerOpeningData().WindowID,
-		ServerSide: false,
-	})
-	if err != nil {
-		return false, fmt.Errorf("CloseContainer: %v", err)
+	// if the container is not opened
+	for i := 0; i < ContainerOperationsReTryMaximumCounts; i++ {
+		g.Resources.Container.AwaitChangesBeforeSendingPacket()
+		// await responce before send packet
+		err := g.WritePacket(&packet.ContainerClose{
+			WindowID:   g.Resources.Container.GetContainerOpeningData().WindowID,
+			ServerSide: false,
+		})
+		if err != nil {
+			return false, fmt.Errorf("CloseContainer: %v", err)
+		}
+		// close container
+		g.Resources.Container.AwaitChangesAfterSendingPacket()
+		// wait changes
+		if g.Resources.Container.GetContainerClosingData() == nil {
+			continue
+		}
+		// if unsuccess
+		return true, nil
+		// return
 	}
-	// close container
-	g.Resources.Container.AwaitChangesAfterSendingPacket()
-	// wait changes
-	if g.Resources.Container.GetContainerClosingData() == nil {
-		return false, nil
-	}
-	// if unsuccess
-	return true, nil
+	// close container.
+	// try a maximum of ContainerOperationsReTryMaximumCounts times
+	return false, nil
 	// return
 }
