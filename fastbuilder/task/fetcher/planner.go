4792520bc1f99b1ee9beb059d6cc7eb8034a6ebd
Ruphane <24724395+LNSSPsd@users.noreply.github.com>
2023-09-09 16:48:02 -0700

    Fix #348

--- fastbuilder/task/fetcher/planner.go
+++ fastbuilder/task/fetcher/planner.go
@@ -4,49 +4,47 @@ import (
 	"fmt"
 )
 
-func PlanHopSwapPath(sx, sz, ex, ez, receptRangeByChunk int) (hopPath []*ExportHopPos, allRequiredChunks ExportedChunksMap) {
-	// receptRangeByChunk is how many chunks in x or z a robot can fetch in a specific point
+func PlanHopSwapPath(sx, sz, ex, ez, reachableChunks int) (hopPath []*ExportHopPos, allRequiredChunks ExportedChunksMap) {
+	// reachableChunks is how many chunks in x or z direction can be fetched in a specific point
 	chunkSize := 16
-	receptRange := chunkSize * receptRangeByChunk
+	reachableBlocks := chunkSize * reachableChunks
 	alignSX := ((sx - chunkSize + 1) / chunkSize) * chunkSize
 	alignSZ := ((sz - chunkSize + 1) / chunkSize) * chunkSize
 	alignCEX := ((ex ) / chunkSize) * chunkSize
 	alignCEZ := ((ez ) / chunkSize) * chunkSize
 	alignMEX := ((ex + chunkSize) / chunkSize) * chunkSize
 	alignMEZ := ((ez + chunkSize) / chunkSize) * chunkSize
-	hopXPoints := ((alignMEX - alignSX) + receptRange - 1) / receptRange
-	hopZPoints := ((alignMEZ - alignSZ) + receptRange - 1) / receptRange
-	// fmt.Println(alignSX, alignSZ, alignCEX, alignCEZ)
-	// fmt.Println(alignSX, alignSZ, alignMEX, alignMEZ)
-	// fmt.Println(hopXPoints, hopZPoints)
+	hopXPoints := ((alignMEX - alignSX) + reachableBlocks - 1) / reachableBlocks
+	hopZPoints := ((alignMEZ - alignSZ) + reachableBlocks - 1) / reachableBlocks
 	hopXArray := []int{}
 	hopZArray := []int{}
-	preferHalfHopXSpace := int(float32(alignMEX-alignSX) / float32(hopXPoints*2))
-	preferHalfHopZSpace := int(float32(alignMEZ-alignSZ) / float32(hopZPoints*2))
-	hopXStart := alignSX + preferHalfHopXSpace
-	hopZStart := alignSZ + preferHalfHopZSpace
+	preferredHalfHopXSpace := int(float32(alignMEX-alignSX) / float32(hopXPoints*2))
+	preferredHalfHopZSpace := int(float32(alignMEZ-alignSZ) / float32(hopZPoints*2))
+	hopXOrigin := alignSX + preferredHalfHopXSpace
+	hopZOrigin := alignSZ + preferredHalfHopZSpace
 	{
 		for i := 0; i < hopXPoints; i++ {
-			hopPoint := hopXStart + i*2*preferHalfHopXSpace
+			hopPoint := hopXOrigin + i*2*preferredHalfHopXSpace
+			//fmt.Printf("NEW hopPoint (X+) %d\n", hopPoint)
 			hopXArray = append(hopXArray, hopPoint)
 		}
 	}
 	{
 		for i := 0; i < hopZPoints; i++ {
-			hopPoint := hopZStart + i*2*preferHalfHopZSpace
+			hopPoint := hopZOrigin + i*2*preferredHalfHopZSpace
+			//fmt.Printf("NEW hopPoint (Z+) %d\n", hopPoint)
 			hopZArray = append(hopZArray, hopPoint)
 		}
 	}
 	hopPoints := []*ExportHopPos{}
-	hopPointsLookUp := map[ChunkPosDefine]*ExportHopPos{}
-	// snake folding
+	hopPointsMap := map[ChunkPosDefine]*ExportHopPos{}
 	for i, x := range hopXArray {
 		if i%2 == 0 {
 			for _, z := range hopZArray {
 				p := ChunkPosDefine{x, z}
 				hp := &ExportHopPos{Pos: p, LinkedChunk: make([]*ExportedChunkPos, 0)}
 				hopPoints = append(hopPoints, hp)
-				hopPointsLookUp[p] = hp
+				hopPointsMap[p] = hp
 			}
 		} else {
 			for zi, _ := range hopZArray {
@@ -54,26 +52,27 @@ func PlanHopSwapPath(sx, sz, ex, ez, receptRangeByChunk int) (hopPath []*ExportH
 				p := ChunkPosDefine{x, z}
 				hp := &ExportHopPos{Pos: p, LinkedChunk: make([]*ExportedChunkPos, 0)}
 				hopPoints = append(hopPoints, hp)
-				hopPointsLookUp[p] = hp
+				hopPointsMap[p] = hp
 			}
 		}
 	}
 	chunkPosMap := ExportedChunksMap{}
-	for xi := alignSX / chunkSize; xi <= alignCEX/chunkSize; xi++ {
-		for zi := alignSZ / chunkSize; zi <= alignCEZ/chunkSize; zi++ {
+	for xi := alignSX / chunkSize; xi < alignCEX/chunkSize; xi++ {
+		for zi := alignSZ / chunkSize; zi < alignCEZ/chunkSize; zi++ {
 			x, z := xi*chunkSize, zi*chunkSize
-			// fmt.Println(x,z)
-			xHalfHops := ((x - alignSX) / preferHalfHopXSpace)
-			hopXPoint := hopXStart + (xHalfHops/2)*2*preferHalfHopXSpace
-			zHalfHops := ((z - alignSZ) / preferHalfHopZSpace)
-			hopZPoint := hopZStart + (zHalfHops/2)*2*preferHalfHopZSpace
+			xHalfHops := ((x - alignSX) / preferredHalfHopXSpace)
+			hopXPoint := hopXOrigin + (xHalfHops/2)*2*preferredHalfHopXSpace
+			zHalfHops := ((z - alignSZ) / preferredHalfHopZSpace)
+			hopZPoint := hopZOrigin + (zHalfHops/2)*2*preferredHalfHopZSpace
 			chunkPos := &ExportedChunkPos{
-				Pos:          ChunkPosDefine{x, z},
-				MasterHop:  hopPointsLookUp[ChunkPosDefine{hopXPoint, hopZPoint}],
+				Pos: ChunkPosDefine{x, z},
+				MasterHop: hopPointsMap[ChunkPosDefine{hopXPoint, hopZPoint}],
 				CachedMark: false,
 			}
 			chunkPosMap[ChunkPosDefine{x, z}] = chunkPos
-			hopPointsLookUp[ChunkPosDefine{hopXPoint, hopZPoint}].LinkedChunk = append(hopPointsLookUp[ChunkPosDefine{hopXPoint, hopZPoint}].LinkedChunk, chunkPos)
+			//fmt.Printf("x=%d, z=%d\n", x, z)
+			//fmt.Printf("REACHING hopPoint (%d, %d)\n", hopXPoint, hopZPoint)
+			hopPointsMap[ChunkPosDefine{hopXPoint, hopZPoint}].LinkedChunk = append(hopPointsMap[ChunkPosDefine{hopXPoint, hopZPoint}].LinkedChunk, chunkPos)
 		}
 	}
 	return hopPoints, chunkPosMap
