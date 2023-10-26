7c9aebcb852ddd84232729a4cf9e79a1e615cbaa
Liliya233 <79683806+Liliya233@users.noreply.github.com>
2023-10-26 21:34:27 +0800

    Fix error print (#353)

--- fastbuilder/pv4/nemc_access_wrapper.go
+++ fastbuilder/pv4/nemc_access_wrapper.go
@@ -32,22 +32,25 @@ func NewAccessWrapper(Client *Client, ServerCode, ServerPassword, Token, usernam
 func (aw *AccessWrapper) GetAccess(ctx context.Context, publicKey []byte) (address string, chainInfo string, err error) {
 	pubKeyData := base64.StdEncoding.EncodeToString(publicKey)
 	chainAddr, ip, token, err := aw.Client.Auth(ctx, aw.ServerCode, aw.ServerPassword, pubKeyData, aw.Token, aw.Username, aw.Password)
+	if err != nil {
+		return "", "", err
+	}
 	if len(token) != 0 {
 		homedir, err := os.UserHomeDir()
 		if err != nil {
 			fmt.Println(I18n.T(I18n.Warning_UserHomeDir))
 			homedir = "."
 		}
-		fbconfigdir := filepath.Join(homedir, ".config/fastbuilder")
+		fbconfigdir := filepath.Join(homedir, ".config", "fastbuilder")
 		os.MkdirAll(fbconfigdir, 0755)
 		ptoken := filepath.Join(fbconfigdir, "fbtoken")
 		// 0600: -rw-------
-		token_file, err:=os.OpenFile(ptoken, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
-		if err!=nil {
+		token_file, err := os.OpenFile(ptoken, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+		if err != nil {
 			return "", "", err
 		}
-		_, err=token_file.WriteString(token)
-		if err!=nil {
+		_, err = token_file.WriteString(token)
+		if err != nil {
 			return "", "", err
 		}
 		token_file.Close()
