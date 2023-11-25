8b6edb96c53a71fae9f8e2ab62e65441a03c4db9
Eternal Crystal <109064184+Happy2018new@users.noreply.github.com>
2023-11-25 21:21:22 +0800

    Delete chinese support (#354)
    close #298

--- fastbuilder/i18n/init.go
+++ fastbuilder/i18n/init.go
@@ -1,36 +1,54 @@
+//go:build !fyne_gui
 // +build !fyne_gui
 
 package I18n
 
 import (
+	"bufio"
 	"fmt"
-	"io/ioutil"
 	"os"
+
+	"github.com/pterm/pterm"
 )
 
 func Init() {
-	config:=loadConfigPath()
-	if _, err:=os.Stat(config); os.IsNotExist(err) {
+	config := loadConfigPath()
+	if _, err := os.Stat(config); os.IsNotExist(err) {
 		SelectLanguage()
-	}else{
-		content, err:=ioutil.ReadFile(config)
-		if (err != nil) {
+	} else {
+		content, err := os.ReadFile(config)
+		if err != nil {
 			fmt.Printf("WARNING: Language config file isn't accessible\n")
-			I18nDict=LangDict["en_US"]
+			I18nDict = LangDict["en_US"]
 			return
 		}
-		langCode:=string(content)
-		SelectedLanguage=langCode
+		langCode := string(content)
+		SelectedLanguage = langCode
 	}
 	langdict, aru := LangDict[SelectedLanguage]
-	if(!aru) {
+	is_deprecated := IsDeprecated(SelectedLanguage)
+	if is_deprecated {
+		pterm.Warning.Printf("The language named `%s` has been removed and will be redirected to `%s`.\nPress Enter to continue.\n", SelectedLanguage, DefaultLanguage)
+		langdict = LangDict["en_US"]
+		if file, err := os.Create(config); err != nil {
+			fmt.Println(T(Lang_Config_ErrOnCreate), err)
+			fmt.Println(T(ErrorIgnored))
+		} else {
+			_, err = file.WriteString(DefaultLanguage)
+			if err != nil {
+				fmt.Println(T(Lang_Config_ErrOnSave), err)
+				fmt.Println(T(ErrorIgnored))
+			}
+			file.Close()
+		}
+		bufio.NewReader(os.Stdin).ReadString('\n')
+	} else if !aru {
 		fmt.Printf("Ordered language doesn't exist.\nPlease reselect one:\n")
 		SelectLanguage()
-		langdict, aru=LangDict[SelectedLanguage]
+		langdict, aru = LangDict[SelectedLanguage]
 		if !aru {
 			panic("Language still unexists after reselection")
-			return
 		}
 	}
-	I18nDict=langdict
+	I18nDict = langdict
 }
