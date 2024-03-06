37e1ead1f9196c9d9daad393f032bded3ef68f1c
Happy2018new <chenbo79800@163.com>
2024-03-06 23:15:47 +0800

    [WIP] reconstruct py rpc related codes [3rd]
    progress <60%

--- fastbuilder/py_rpc/mod_event_server_to_client/minecraft.go
+++ fastbuilder/py_rpc/mod_event_server_to_client/minecraft.go
@@ -1,12 +1,12 @@
 package mod_event_server_to_client
 
 import (
-	"phoenixbuilder/fastbuilder/py_rpc/mod_event_server_to_client/general_interface"
+	"phoenixbuilder/fastbuilder/py_rpc/general_interface/mod_event"
 	"phoenixbuilder/fastbuilder/py_rpc/mod_event_server_to_client/minecraft"
 )
 
 // Minecraft Package
-type Minecraft struct{ general_interface.Module }
+type Minecraft struct{ mod_event.Default }
 
 // Return the package name of m
 func (m *Minecraft) PackageName() string {
@@ -14,8 +14,8 @@ func (m *Minecraft) PackageName() string {
 }
 
 // Return a pool/map that contains all the module of m
-func (m *Minecraft) ModulePool() map[string]general_interface.Module {
-	return map[string]general_interface.Module{
-		"aiCommand": &minecraft.AICommand{},
+func (m *Minecraft) ModulePool() map[string]mod_event.Module {
+	return map[string]mod_event.Module{
+		"aiCommand": &minecraft.AICommand{Module: &mod_event.DefaultModule{}},
 	}
 }
