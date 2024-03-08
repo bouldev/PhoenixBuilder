c913d20821d21a9023913e374a0fd5dcf579b83e
Happy2018new <chenbo79800@163.com>
2024-03-08 22:44:18 +0800

    add missing field
    "PyRpc/S2CModEvent/minecraft/chatPhrases/SyncNewPlayerPhrasesData" related

--- fastbuilder/py_rpc/py_rpc_packet/mod_event_server_to_client/minecraft/chat_phrases/sync_new_player_phrases_data.go
+++ fastbuilder/py_rpc/py_rpc_packet/mod_event_server_to_client/minecraft/chat_phrases/sync_new_player_phrases_data.go
@@ -6,7 +6,11 @@ const (
 	SourceOfficial = "OFFICIAL_SIGN"
 )
 
-type SyncNewPlayerPhrasesData []PhrasesData
+type SyncNewPlayerPhrasesData struct {
+	Data []PhrasesData `json:"phrasesData"` // ...
+	Show bool          `json:"show"`        // e.g. true
+}
+
 type PhrasesData struct {
 	Source  string `json:"itemId"`  // e.g. "OFFICIAL_SIGN"
 	ID      int64  `json:"id"`      // e.g. int64(1002)
@@ -21,7 +25,7 @@ func (s *SyncNewPlayerPhrasesData) EventName() string {
 // Convert s to go object which only contains go-built-in types
 func (s *SyncNewPlayerPhrasesData) MakeGo() (res any) {
 	new := map[string]any{}
-	for _, value := range *s {
+	for _, value := range s.Data {
 		_, ok := new[value.Source]
 		if !ok {
 			new[value.Source] = make(map[string]any)
@@ -32,7 +36,10 @@ func (s *SyncNewPlayerPhrasesData) MakeGo() (res any) {
 			"itemId":  value.Source,
 		}
 	}
-	return map[string]any{"phrasesData": new}
+	return map[string]any{
+		"phrasesData": new,
+		"show":        s.Show,
+	}
 }
 
 // Sync data to s from obj
@@ -42,48 +49,56 @@ func (s *SyncNewPlayerPhrasesData) FromGo(obj any) error {
 		return fmt.Errorf("FromGo: Failed to convert obj to map[string]interface{}; obj = %#v", obj)
 	}
 	// convert data
-	phrases_data, success := object["phrasesData"].(map[string]any)
-	if !success {
-		return fmt.Errorf(`FromGo: Failed to convert object["phrasesData"] to map[string]interface{}; object["phrasesData"] = %#v`, object["phrasesData"])
-	}
-	// get phrases data
-	*s = make(SyncNewPlayerPhrasesData, 0)
-	// make object
-	for source_name, multiple_phrases := range phrases_data {
-		mp, success := multiple_phrases.(map[string]any)
+	{
+		s.Data = make([]PhrasesData, 0)
+		// make object
+		phrases_data, success := object["phrasesData"].(map[string]any)
 		if !success {
-			return fmt.Errorf(`FromGo: Failed to convert phrases_data[%#v] to map[string]interface{}; phrases_data[%#v] = %#v`, source_name, source_name, multiple_phrases)
+			return fmt.Errorf(`FromGo: Failed to convert object["phrasesData"] to map[string]interface{}; object["phrasesData"] = %#v`, object["phrasesData"])
 		}
-		// convert data
-		for id, phrases := range mp {
-			p, success := phrases.(map[string]any)
+		// get phrases data
+		for source_name, multiple_phrases := range phrases_data {
+			mp, success := multiple_phrases.(map[string]any)
 			if !success {
-				return fmt.Errorf(`FromGo: Failed to convert mp[%#v] to map[string]interface{}; mp[%#v] = %#v`, id, id, phrases)
+				return fmt.Errorf(`FromGo: Failed to convert phrases_data[%#v] to map[string]interface{}; phrases_data[%#v] = %#v`, source_name, source_name, multiple_phrases)
 			}
 			// convert data
-			content, success := p["content"].(string)
-			if !success {
-				return fmt.Errorf(`FromGo: Failed to convert p["content"] to string; p = %#v`, p)
+			for id, phrases := range mp {
+				p, success := phrases.(map[string]any)
+				if !success {
+					return fmt.Errorf(`FromGo: Failed to convert mp[%#v] to map[string]interface{}; mp[%#v] = %#v`, id, id, phrases)
+				}
+				// convert data
+				content, success := p["content"].(string)
+				if !success {
+					return fmt.Errorf(`FromGo: Failed to convert p["content"] to string; p = %#v`, p)
+				}
+				id_converted, success := p["id"].(int64)
+				if !success {
+					return fmt.Errorf(`FromGo: Failed to convert p["id"] to int64; p = %#v`, p)
+				}
+				item_id, success := p["itemId"].(string)
+				if !success {
+					return fmt.Errorf(`FromGo: Failed to convert p["itemId"] to string; p = %#v`, p)
+				}
+				// get data
+				s.Data = append(s.Data, PhrasesData{
+					Source:  item_id,
+					ID:      id_converted,
+					Content: content,
+				})
+				// submit sub result
 			}
-			id_converted, success := p["id"].(int64)
-			if !success {
-				return fmt.Errorf(`FromGo: Failed to convert p["id"] to int64; p = %#v`, p)
-			}
-			item_id, success := p["itemId"].(string)
-			if !success {
-				return fmt.Errorf(`FromGo: Failed to convert p["itemId"] to string; p = %#v`, p)
-			}
-			// get data
-			*s = append(*s, PhrasesData{
-				Source:  item_id,
-				ID:      id_converted,
-				Content: content,
-			})
-			// submit sub result
+			// for each source, like "OFFICIAL_SIGN"
 		}
-		// for each source, like "OFFICIAL_SIGN"
+		// sync phrases data
 	}
-	// sync data
+	// get and sync phrases data
+	s.Show, success = object["show"].(bool)
+	if !success {
+		return fmt.Errorf(`FromGo: Failed to convert object["show"] to bool; object["show"] = %#v`, object["show"])
+	}
+	// get and sync show data
 	return nil
 	// return
 }
