412adf3528844db05bcd112057cb04eebf38e28d
Happy2018new <chenbo79800@163.com>
2024-03-18 22:34:24 +0800

    move constants (`ResourcesControl` related)

--- game_control/resources_control/resources_control.go
+++ game_control/resources_control/resources_control.go
@@ -36,23 +36,6 @@ type Resources struct {
 
 // ------------------------- commandRequestWithResponce -------------------------
 
-// 描述命令请求的原始类型
-const (
-	// 标准命令
-	CommandTypeStandard = "packet.CommandRequest"
-	// 网易魔法指令
-	CommandTypeAICommand = "packet.PyRpc/C2SModEvent/Minecraft/aiCommand/ExecuteCommandEvent"
-)
-
-// 描述与命令请求相关的信号
-const (
-	// 魔法指令 的命令请求在收到
-	// 标准响应体后会使用的信号
-	SignalRespondReceived = uint8(iota)
-	// 命令响应体可以被加载时会使用的信号
-	SignalCouldLoadRespond
-)
-
 /*
 存放命令请求及结果
 
