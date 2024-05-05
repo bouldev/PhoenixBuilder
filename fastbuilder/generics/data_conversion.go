5a2f24bd403541b5f2618a61ec11a4c56ffd795e
Happy2018new <chenbo79800@163.com>
2024-05-05 12:44:48 +0800

    completely data conversion

--- fastbuilder/generics/data_conversion.go
+++ fastbuilder/generics/data_conversion.go
@@ -2,11 +2,17 @@ package generics
 
 import "fmt"
 
-// Convert map[K0]V0 to map[K1]V1
+/*
+将 mapping(map[K0]V0) 转换为 map[K1]V1 。
+
+var_name 指代 mapping 在源代码中的实际名称；
+path 指代调用此函数的代码在源代码的路径。
+以上两个参数被共同用于追踪引发转换错误的位置
+*/
 func Map[
 	K0 comparable, V0 any,
 	K1 comparable, V1 any,
-](mapping map[K0]V0) (
+](mapping map[K0]V0, var_name string, path string) (
 	result map[K1]V1,
 	err error,
 ) {
@@ -18,12 +24,12 @@ func Map[
 		// prepare
 		k, success := K.(K1)
 		if !success {
-			return nil, fmt.Errorf("Map: Failed to convert key(%T) to type %T; key = %#v, mapping = %#v", key, k, key, mapping)
+			return nil, fmt.Errorf("Map: Failed to convert key(%T) to type %T; key = %#v, %s = %#v; path = %s", key, k, key, var_name, mapping, path)
 		}
 		// convert key
 		v, success := V.(V1)
 		if !success {
-			return nil, fmt.Errorf("Map: Failed to convert mapping[%#v](%T) to type %T; mapping = %#v", k, value, v, mapping)
+			return nil, fmt.Errorf("Map: Failed to convert %s[%#v](%T) to type %T; %s = %#v; path = %s", var_name, k, value, v, var_name, mapping, path)
 		}
 		// convert value
 		result[k] = v
@@ -34,8 +40,14 @@ func Map[
 	// return
 }
 
-// Convert []V0 to []V1
-func Slice[V0 any, V1 any](slice []V0) (
+/*
+将 slice([]V0) 转换为 []V1 。
+
+var_name 指代 mapping 在源代码中的实际名称；
+path 指代调用此函数的代码在源代码的路径。
+以上两个参数被共同用于追踪引发转换错误的位置
+*/
+func Slice[V0 any, V1 any](slice []V0, var_name string, path string) (
 	result []V1,
 	err error,
 ) {
@@ -46,7 +58,7 @@ func Slice[V0 any, V1 any](slice []V0) (
 		// prepare
 		v, success := val.(V1)
 		if !success {
-			return nil, fmt.Errorf("Slice: Failed to convert slice[%d](%T) to type %T; slice = %#v", index, value, v, slice)
+			return nil, fmt.Errorf("Slice: Failed to convert %s[%d](%T) to type %T; %s = %#v; path = %s", var_name, index, value, v, var_name, slice, path)
 		}
 		// convert data
 		result[index] = v
@@ -58,10 +70,20 @@ func Slice[V0 any, V1 any](slice []V0) (
 }
 
 // Convert value to type V
-func To[V any](value any) (result V, err error) {
+/*
+将 value 转换为类型为 V 的变量。
+
+var_name 指代 mapping 在源代码中的实际名称；
+path 指代调用此函数的代码在源代码的路径。
+以上两个参数被共同用于追踪引发转换错误的位置
+*/
+func To[V any](value any, var_name string, path string) (
+	result V,
+	err error,
+) {
 	result, success := value.(V)
 	if !success {
-		err = fmt.Errorf("To: Failed to convert value(%T) to type %T; value = %#v", value, result, value)
+		err = fmt.Errorf("To: Failed to convert %s(%T) to type %T; %s = %#v; path = %s", var_name, value, result, var_name, value, path)
 	}
 	return
 }
