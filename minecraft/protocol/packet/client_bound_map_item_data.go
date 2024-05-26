018b21000cbe39bca94eb38a99a542c7b4583928
Happy2018new <chenbo79800@163.com>
2024-05-26 13:50:29 +0800

    provide support for pk `ClientBoundMapItemData`
    compatibility is not guaranteed in all cases, as this is a modification made through experimental observations.

--- minecraft/protocol/packet/client_bound_map_item_data.go
+++ minecraft/protocol/packet/client_bound_map_item_data.go
@@ -1,9 +1,19 @@
 package packet
 
 import (
-	"phoenixbuilder/minecraft/protocol"
+	"fmt"
 	"image/color"
 	"math"
+	"phoenixbuilder/minecraft/protocol"
+)
+
+// 一些魔法字段，
+// 看起来像是用于描述是否需要继续 编码/解码 地图数据。
+// 它们的作用仍是未知的，这也包括它们的数据类型，
+// 因为这一切都仅仅是通过实验观察得到的结论
+const (
+	MapDataContinue = uint8(iota)
+	MapDataTerminate
 )
 
 const (
@@ -65,9 +75,32 @@ type ClientBoundMapItemData struct {
 	// YOffset is the Y offset in pixels at which the updated texture area starts. From this Y, the updated
 	// texture will extend exactly Height pixels up.
 	YOffset int32
-	// Pixels is a list of pixel colours for the new texture of the map. It is indexed as Pixels[y][x], with
-	// the length of the outer slice having to be exactly Height long and the inner slices exactly Width long.
-	Pixels [][]color.RGBA
+	/*
+		一个切片，但实际作用是映射，
+		用于表示数字和颜色的对应关系，它并非是恒定不变的。
+
+		例如，用切片的第 0 项和第 1 项分别代表 黄色 和 黑色，
+		或在下次使用切片的 0 项和第 1 项分别代表 绿色 和 橙色。
+
+		这个映射表被用于下方的 Pixels 二维数字矩阵，
+		这个矩阵中的每个数字就代表一个颜色。因此，
+		我们通过使用 ColorMap 来描述 Pixels 中数字和颜色的对应关系。
+	*/
+	ColorMap []color.RGBA
+	/*
+		Pixels is a list of pixel colours for the new texture of the map. It is indexed as Pixels[y][x], with
+		the length of the outer slice having to be exactly Height long and the inner slices exactly Width long.
+
+		Pixels 中的数字被表示为相应的颜色，
+		这个颜色可以通过上方的 ColorMap 来查找对应，
+		即“目标颜色 = ColorMap[数字]”。
+
+		需要说明的是，被用于表示颜色的 uint32 是不确定的，
+		这个数据类型仅仅是一个未经验证的推断 [需要更多信息]
+	*/
+	Pixels [][]uint32
+	// Origin is the center position of the map being updated.
+	Origin protocol.BlockPos
 }
 
 // ID ...
@@ -77,10 +110,14 @@ func (*ClientBoundMapItemData) ID() uint32 {
 
 // Marshal ...
 func (pk *ClientBoundMapItemData) Marshal(w *protocol.Writer) {
+	var magic_mark_0 uint8 = MapDataContinue
+	var magic_mark_1 uint8 = 1
+
 	w.Varint64(&pk.MapID)
 	w.Varuint32(&pk.UpdateFlags)
 	w.Uint8(&pk.Dimension)
 	w.Bool(&pk.LockedMap)
+	w.BlockPos(&pk.Origin)
 
 	if pk.UpdateFlags&MapUpdateFlagInitialisation != 0 {
 		l := uint32(len(pk.MapsIncludedIn))
@@ -89,9 +126,11 @@ func (pk *ClientBoundMapItemData) Marshal(w *protocol.Writer) {
 			w.Varint64(&mapID)
 		}
 	}
+
 	if pk.UpdateFlags&(MapUpdateFlagInitialisation|MapUpdateFlagDecoration|MapUpdateFlagTexture) != 0 {
 		w.Uint8(&pk.Scale)
 	}
+
 	if pk.UpdateFlags&MapUpdateFlagDecoration != 0 {
 		l := uint32(len(pk.TrackedObjects))
 		w.Varuint32(&l)
@@ -104,6 +143,7 @@ func (pk *ClientBoundMapItemData) Marshal(w *protocol.Writer) {
 			protocol.MapDeco(w, &decoration)
 		}
 	}
+
 	if pk.UpdateFlags&MapUpdateFlagTexture != 0 {
 		// Some basic validation for the values passed into the packet.
 		if pk.Width <= 0 || pk.Height <= 0 {
@@ -114,6 +154,8 @@ func (pk *ClientBoundMapItemData) Marshal(w *protocol.Writer) {
 		w.Varint32(&pk.Height)
 		w.Varint32(&pk.XOffset)
 		w.Varint32(&pk.YOffset)
+		w.Uint8(&magic_mark_0)
+		w.Uint8(&magic_mark_1)
 
 		l := uint32(pk.Width * pk.Height)
 		w.Varuint32(&l)
@@ -126,22 +168,31 @@ func (pk *ClientBoundMapItemData) Marshal(w *protocol.Writer) {
 				panic("invalid map texture update: length of inner pixels array must be equal to width")
 			}
 			for x := int32(0); x < pk.Width; x++ {
-				w.VarRGBA(&pk.Pixels[y][x])
+				w.Uint32(&pk.Pixels[y][x])
 			}
+		}
+
+		l = uint32(len(pk.ColorMap))
+		w.Varuint32(&l)
+		for i := uint32(0); i < l; i++ {
+			w.NeteaseRGBA(&pk.ColorMap[i])
+			w.Varuint32(&i)
 		}
 	}
 }
 
 // Unmarshal ...
 func (pk *ClientBoundMapItemData) Unmarshal(r *protocol.Reader) {
-	// IGNORED !!!
-	return
+	var count uint32
+	var magic_mark_0 uint8
+	var magic_mark_1 uint8
+
 	r.Varint64(&pk.MapID)
 	r.Varuint32(&pk.UpdateFlags)
 	r.Uint8(&pk.Dimension)
 	r.Bool(&pk.LockedMap)
+	r.BlockPos(&pk.Origin)
 
-	var count uint32
 	if pk.UpdateFlags&MapUpdateFlagInitialisation != 0 {
 		r.Varuint32(&count)
 		pk.MapsIncludedIn = make([]int64, count)
@@ -149,9 +200,11 @@ func (pk *ClientBoundMapItemData) Unmarshal(r *protocol.Reader) {
 			r.Varint64(&pk.MapsIncludedIn[i])
 		}
 	}
+
 	if pk.UpdateFlags&(MapUpdateFlagInitialisation|MapUpdateFlagDecoration|MapUpdateFlagTexture) != 0 {
 		r.Uint8(&pk.Scale)
 	}
+
 	if pk.UpdateFlags&MapUpdateFlagDecoration != 0 {
 		r.Varuint32(&count)
 		pk.TrackedObjects = make([]protocol.MapTrackedObject, count)
@@ -164,23 +217,48 @@ func (pk *ClientBoundMapItemData) Unmarshal(r *protocol.Reader) {
 			protocol.MapDeco(r, &pk.Decorations[i])
 		}
 	}
+
 	if pk.UpdateFlags&MapUpdateFlagTexture != 0 {
 		r.Varint32(&pk.Width)
 		r.Varint32(&pk.Height)
 		r.Varint32(&pk.XOffset)
 		r.Varint32(&pk.YOffset)
+		{
+			r.Uint8(&magic_mark_0)
+			if magic_mark_0 == MapDataTerminate {
+				return
+			}
+			r.Uint8(&magic_mark_1)
+			if !(magic_mark_0 == MapDataContinue && magic_mark_1 == 1) {
+				panic(fmt.Sprintf(
+					"(pk *ClientBoundMapItemData) Unmarshal: Magic mark not matched, expect %#v but case %#v.",
+					[]byte{MapDataContinue, 1},
+					[]byte{magic_mark_0, magic_mark_1},
+				))
+			}
+		}
 		r.Varuint32(&count)
 
 		r.LimitInt32(pk.Width, 0, math.MaxInt16)
 		r.LimitInt32(pk.Height, 0, math.MaxInt16)
 		r.LimitInt32(pk.Width*pk.Height, int32(count), int32(count))
 
-		pk.Pixels = make([][]color.RGBA, pk.Height)
+		pk.Pixels = make([][]uint32, pk.Height)
 		for y := int32(0); y < pk.Height; y++ {
-			pk.Pixels[y] = make([]color.RGBA, pk.Width)
+			pk.Pixels[y] = make([]uint32, pk.Width)
 			for x := int32(0); x < pk.Width; x++ {
-				r.VarRGBA(&pk.Pixels[y][x])
+				r.Varuint32(&pk.Pixels[y][x])
 			}
+		}
+
+		r.Varuint32(&count)
+		pk.ColorMap = make([]color.RGBA, count)
+		for i := uint32(0); i < count; i++ {
+			var rgba color.RGBA
+			var key uint32
+			r.NeteaseRGBA(&rgba)
+			r.Varuint32(&key)
+			pk.ColorMap[key] = rgba
 		}
 	}
 }
