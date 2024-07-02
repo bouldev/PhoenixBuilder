91e37f130f79c5fb09f9a761dd8712f628e2cde0
Happy2018new <chenbo79800@163.com>
2024-07-02 15:16:37 +0800

    fix resource pack size mismatch
    origin title:
    disable error print when resource pack size mismatch
    
    sync this from https://github.com/bouldev/neo-omega-kernel/commit/a0a56b39861717461a456ca122545fe4eb8797c6
    
    Co-Authored-By: Liliya233 <79683806+Liliya233@users.noreply.github.com>

--- minecraft/conn.go
+++ minecraft/conn.go
@@ -1154,7 +1154,17 @@ func (conn *Conn) handleResourcePackDataInfo(pk *packet.ResourcePackDataInfo) er
 	if pack.size != pk.Size {
 		// Size mismatch: The ResourcePacksInfo packet had a size for the pack that did not match with the
 		// size sent here.
-		conn.log.Printf("pack %v had a different size in the ResourcePacksInfo packet than the ResourcePackDataInfo packet\n", id)
+
+		// PhoenixBuilder specific changes.
+		// Author: Liliya233
+		//
+		// Netease: disable error log as they're encrypted
+		{
+			// Size mismatch: The ResourcePacksInfo packet had a size for the pack that did not match with the
+			// size sent here.
+			//
+			// conn.log.Printf("pack %v had a different size in the ResourcePacksInfo packet than the ResourcePackDataInfo packet\n", id)
+		}
 		pack.size = pk.Size
 	}
 
