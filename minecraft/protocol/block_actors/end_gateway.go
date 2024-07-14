ae20289ce72af9094bc67f062f16ad60f1410e17
Happy2018new <chenbo79800@163.com>
2024-07-14 10:51:48 +0800

    not very necessary to omitempty for `ExitPortal`
    block actor "end_gateway" related

--- minecraft/protocol/block_actors/end_gateway.go
+++ minecraft/protocol/block_actors/end_gateway.go
@@ -8,8 +8,8 @@ import (
 // 末地折跃门方块
 type EndGateway struct {
 	general.BlockActor `mapstructure:",squash"`
-	Age                int32   `mapstructure:"Age"`                  // TAG_Int(4) = 0
-	ExitPortal         []int32 `mapstructure:"ExitPortal,omitempty"` // TAG_List[TAG_Int] (9[4])
+	Age                int32   `mapstructure:"Age"`        // TAG_Int(4) = 0
+	ExitPortal         []int32 `mapstructure:"ExitPortal"` // TAG_List[TAG_Int] (9[4])
 }
 
 // ID ...
