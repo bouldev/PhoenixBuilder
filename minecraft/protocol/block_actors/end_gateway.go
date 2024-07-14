443b5cc5df06e0f5e1fb467d4811556f88e3f599
Happy2018new <chenbo79800@163.com>
2024-07-14 10:21:19 +0800

    optimize end gateway NBT decode

--- minecraft/protocol/block_actors/end_gateway.go
+++ minecraft/protocol/block_actors/end_gateway.go
@@ -8,8 +8,8 @@ import (
 // 末地折跃门方块
 type EndGateway struct {
 	general.BlockActor `mapstructure:",squash"`
-	Age                int32 `mapstructure:"Age"`                  // TAG_Int(4) = 0
-	ExitPortal         []any `mapstructure:"ExitPortal,omitempty"` // TAG_List[TAG_Int] (9[4])
+	Age                int32   `mapstructure:"Age"`                  // TAG_Int(4) = 0
+	ExitPortal         []int32 `mapstructure:"ExitPortal,omitempty"` // TAG_List[TAG_Int] (9[4])
 }
 
 // ID ...
@@ -20,5 +20,5 @@ func (*EndGateway) ID() string {
 func (b *EndGateway) Marshal(io protocol.IO) {
 	protocol.Single(io, &b.BlockActor)
 	io.Varint32(&b.Age)
-	protocol.NBTSlice(io, &b.ExitPortal, func(t *[]int32) { protocol.FuncSliceOfLen(io, 3, t, io.Varint32) })
+	protocol.FuncSliceOfLen(io, 3, &b.ExitPortal, io.Varint32)
 }
