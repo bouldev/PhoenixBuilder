34eb0cfb096e6c5d66a43fb03ec51f2d77c38652
Happy2018new <chenbo79800@163.com>
2024-07-15 10:38:41 +0800

    fix a bug (auth server address related)

--- fastbuilder/args/args.c
+++ fastbuilder/args/args.c
@@ -36,7 +36,7 @@ struct go_string {
 char args_isDebugMode = 0;
 char args_disableVersionChecking = 0;
 char args_skipMCPCheckChallenges = 0;
-struct go_string newAuthServer = {"https://user.fastbuilder.pro", 27};
+struct go_string newAuthServer = {"https://user.fastbuilder.pro", 28}; // 28 is refer to the length of the URL address of auth server
 struct go_string startup_script = EMPTY_GOSTRING;
 struct go_string server_code = EMPTY_GOSTRING;
 struct go_string server_password = EMPTY_GOSTRING;
