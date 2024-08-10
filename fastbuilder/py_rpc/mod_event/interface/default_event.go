70fb58f1a26ef0d537ccb4c11e0609e52dbc3994
Happy2018new <chenbo79800@163.com>
2024-08-10 19:28:34 +0800

    PyRpc: Fix failed to unmarshal mod event

--- fastbuilder/py_rpc/mod_event/interface/default_event.go
+++ fastbuilder/py_rpc/mod_event/interface/default_event.go
@@ -1,12 +1,10 @@
 package mod_event_interface
 
-import "fmt"
-
 // Default event, which used to
 // describe the unsupported parts
 type DefaultEvent struct {
-	EVENT_NAME string         // The name of this event
-	Data       map[string]any // The data contained in this event
+	EVENT_NAME string // The name of this event
+	Data       any    // The data contained in this event
 }
 
 // Return the event name of d
@@ -21,12 +19,6 @@ func (d *DefaultEvent) MakeGo() (res any) {
 
 // Sync data to d from obj
 func (d *DefaultEvent) FromGo(res any) error {
-	result, success := res.(map[string]any)
-	if !success {
-		return fmt.Errorf("FromGo: Failed to covert res to map[string]interface{}; res = %#v", res)
-	}
-	// convert data
-	d.Data = result
+	d.Data = res
 	return nil
-	// sync data and return
 }
